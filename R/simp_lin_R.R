7111cf4d31a3e180e272edea38e1576ed5602178
MantautasRimkus <mantautas.rimkus@gmail.com>
2021-01-26 22:00:13 -0700

    Update1

--- R/simp_lin_R.R
+++ R/simp_lin_R.R
@@ -1,3 +1,21 @@
+#' Simple linear regression
+#'
+#' Takes two numeric vectors and gives results of linear regression
+#'
+#' @param y is numeric vector known as response
+#' @param x is numeric vector known as predictor. x length must be equal to y length
+#'
+#' @return Returns regression coefficients, their corresponding standard errors and 95pct conﬁdence intervals, residuals, and predicted values as a list
+#'
+#' @author Mantautas Rimkus, \email{mantauc@@colostate.edu}
+#' @keywords simple
+#'
+#'
+#' @export
+#' @importFrom Rcpp RcppArmadillo
+#'
+
+
 simp_lin_R <- function(y,x){
   if(!is.numeric(x)) {
     stop("x is not numeric vector")
