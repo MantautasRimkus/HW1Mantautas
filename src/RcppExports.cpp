04d9b44333b2587093fbd6ae2153b5eee6bf2f2b
MantautasRimkus <mantautas.rimkus@gmail.com>
2021-01-28 13:58:43 -0700

    Changing the name of the package

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,52 +6,9 @@
 
 using namespace Rcpp;
 
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _HW1Mantautas_rcpparma_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _HW1Mantautas_rcpparma_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _HW1Mantautas_rcpparma_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _HW1Mantautas_rcpparma_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
 // simp_lin_cpp
 Rcpp::List simp_lin_cpp(arma::vec y, arma::vec x);
-RcppExport SEXP _HW1Mantautas_simp_lin_cpp(SEXP ySEXP, SEXP xSEXP) {
+RcppExport SEXP _Simplin_simp_lin_cpp(SEXP ySEXP, SEXP xSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -63,15 +20,11 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_HW1Mantautas_rcpparma_hello_world", (DL_FUNC) &_HW1Mantautas_rcpparma_hello_world, 0},
-    {"_HW1Mantautas_rcpparma_outerproduct", (DL_FUNC) &_HW1Mantautas_rcpparma_outerproduct, 1},
-    {"_HW1Mantautas_rcpparma_innerproduct", (DL_FUNC) &_HW1Mantautas_rcpparma_innerproduct, 1},
-    {"_HW1Mantautas_rcpparma_bothproducts", (DL_FUNC) &_HW1Mantautas_rcpparma_bothproducts, 1},
-    {"_HW1Mantautas_simp_lin_cpp", (DL_FUNC) &_HW1Mantautas_simp_lin_cpp, 2},
+    {"_Simplin_simp_lin_cpp", (DL_FUNC) &_Simplin_simp_lin_cpp, 2},
     {NULL, NULL, 0}
 };
 
-RcppExport void R_init_HW1Mantautas(DllInfo *dll) {
+RcppExport void R_init_Simplin(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
